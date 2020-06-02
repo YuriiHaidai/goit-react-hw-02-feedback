d77984a0711f2cc25e465b2429faa3e7b141d9e0
Gaiday_u <ura.kiev@gmail.com>
2020-06-02 15:00:35 +0300

    Updates

---
+++ precache-manifest.9fa95f794ead290a1455b593c1761313.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844549679d01d3e8c0b0706ce449da09",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "659d995d80a58fc65245",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9c8ae13a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9c8ae13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24153cda8a3678a3d25d",
+    "url": "/goit-react-hw-02-feedback/static/js/main.e3695167.chunk.js"
+  },
+  {
+    "revision": "530a2ea63a63b985e80c",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.f97b67d2.js"
+  }
+]);
